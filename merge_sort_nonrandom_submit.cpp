b4a109d8c4575011b2f2451841775b0d1c284199
Cascol-SCUT <524048328@qq.com>
2021-07-04 13:09:50 +0800

    const 指针

--- merge_sort_nonrandom_submit.cpp
+++ merge_sort_nonrandom_submit.cpp
@@ -7,12 +7,12 @@ This is parallel merge_sort, the algorithm is as follow
     5. split the array in each process into k segments according to split points and send them to the right process, each process will then have k segments
     6. each process run k-merge algorithm to merge k segments
     7. gather the final answer
-    
+
 author: Cascol-SCUT
 link: https://github.com/Cascol-SCUT/Parallel-Sort
 */
 
-#if 0
+#if 1
 #pragma GCC optimize(1)
 #pragma GCC optimize(2)
 #pragma GCC optimize(3)
@@ -133,8 +133,8 @@ int main(int argc, char *argv[])
     MPI_File_get_size(fh, &ofs);
     const int n = ofs / sizeof(int);
     const int number_per_process = n / num_procs;
-    int sz = (rank == num_procs - 1 ? (n - number_per_process * (num_procs - 1)) : number_per_process);
-    int *a = new int[sz];
+    const int sz = (rank == num_procs - 1 ? (n - number_per_process * (num_procs - 1)) : number_per_process);
+    int * const a = new int[sz];
     MPI_File_read_at(fh, rank * number_per_process * sizeof(int), &a[0], sz, MPI_INT, &status);
 
     MPI_File_close(&fh);
@@ -179,7 +179,7 @@ int main(int argc, char *argv[])
         }
         else
         {
-            auto it2 = upper_bound(a, a + sz, p[i]);
+            const auto it2 = upper_bound(a, a + sz, p[i]);
             int per_num;
             const int total_number = it2 - a - pre_r;
             if (pre_r + total_number / (value_cnt + 1) < it - a)
@@ -219,7 +219,7 @@ int main(int argc, char *argv[])
             displ[i][j] = displ[i][j - 1] + vec_info[i][j - 1];
         }
     }
-    int *recv_seg = new int[total_cnt];
+    int * const recv_seg = new int[total_cnt];
     MPI_Request req[num_procs];
     int l = 0;
     for (int i = 0; i < num_procs; ++i)
@@ -231,7 +231,7 @@ int main(int argc, char *argv[])
 
     // memory management
     delete[] a;
-    int *send = new int[total_cnt];
+    int * const send = new int[total_cnt];
 
     int pl[num_procs], pr[num_procs];
     for (int i = 0; i < num_procs; ++i)
@@ -275,14 +275,12 @@ int main(int argc, char *argv[])
         displ_per_process[i] = displ_per_process[i - 1] + cnt_per_process[i - 1];
 
     delete[] recv_seg;
-    int *ans;
-    ans = (!rank ? new int[n] : nullptr);
+    int * const ans = (!rank ? new int[n] : nullptr);
     MPI_Gatherv(&send[0], total_cnt, MPI_INT, &ans[0], &cnt_per_process[0], &displ_per_process[0], MPI_INT, 0, MPI_COMM_WORLD);
     if (rank == 0)
     {
-        cout << ans[output_index] << "\n";
+        cout<<ans[output_index+1]<<"\n";
     }
-    
     MPI_Finalize();
 }
 /*
